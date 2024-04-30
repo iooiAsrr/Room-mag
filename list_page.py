fd80308435d2808b4f46f21470fe717c85dd2702
Asrr <apologizeya@qq.com>
2024-04-30 13:34:48 +0800

    step7_2

--- list_page.py
+++ list_page.py
@@ -21,7 +21,7 @@ def search_txt_info():
         result = House.query.filter(House.rooms == rooms_info).order_by(House.publish_time.desc()).all()
         return render_template('search_list.html.bp', house_list=result)
         return redirect(url_for('index_page.index'))
-
+#2021011125-杨高磊
 @list_page.route('/list/pattern/<int:page>')
 def return_new_list(page):
     # 获取房源总数量
@@ -29,5 +29,19 @@ def return_new_list(page):
     # 计算总的页码数，向上取整
     total_num = math.ceil(house_num / 10)
     result = House.query.order_by(
-        House.publish_time.desc()).paginate(page, per_page=10)
-    return render_template('list.html.bp', house_list=result.items, page_num=result.page, total_num=total_num)
+        House.publish_time.desc()).paginate(page = page, per_page=10)
+    return render_template('list.html', house_list=result.items, page_num=result.page, total_num=total_num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
