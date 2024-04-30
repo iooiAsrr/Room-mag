3b47a994fba5ad3fe74cfca030afba0012905672
Asrr <apologizeya@qq.com>
2024-04-30 11:51:16 +0800

    step6_1

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from settings import Config, db
 from models import House
+from index_page import index_page
 # 2021011125杨高磊
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
-@app.route('/')
-def test():
-#feasfasef
-    # 查询第一条记录
-    first_user = House.query.first()
-    print(first_user)
-    return 'ok'
+
+# @app.route('/')
+# def test():
+#     # 查询第一条记录
+#     first_user = House.query.first()
+#     print(first_user)
+#     return 'ok'
+
+app.register_blueprint(index_page)
 if __name__ == '__main__':
     app.run(debug=True)
